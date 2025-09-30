05057c8fc07971f704cb482b45c89dcba6df2d2f
piaorongzhen <prz_2022@qq.com>
2025-09-30 15:38:21 +0800

    thrift通信切换国密

--- client/src/main/java/org/example/JavaClient.java
+++ client/src/main/java/org/example/JavaClient.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.tencent.kona.crypto.KonaCryptoProvider;
+import com.tencent.kona.pkix.KonaPKIXProvider;
+import com.tencent.kona.ssl.KonaSSLProvider;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
@@ -11,13 +14,19 @@ import tutorial.InvalidOperation;
 import tutorial.Operation;
 import tutorial.Work;
 
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManagerFactory;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.Security;
+
 public class JavaClient {
     public static void main(String[] args) {
 
         try {
-            TTransport transport;
-            transport = new TSocket("localhost", 9090);
-            transport.open();
+            TTransport transport = new TSocket(getSSLSocket());
 
             TProtocol protocol = new TBinaryProtocol(transport);
             Calculator.Client client = new Calculator.Client(protocol);
@@ -25,7 +34,7 @@ public class JavaClient {
             perform(client);
 
             transport.close();
-        } catch (TException x) {
+        } catch (Exception x) {
             x.printStackTrace();
         }
     }
@@ -61,5 +70,31 @@ public class JavaClient {
 
         SharedStruct log = client.getStruct(1);
         System.out.println("Check log: " + log.value);
+    }
+
+    public static SSLSocket getSSLSocket() throws Exception {
+
+        // 加载国密Provider
+        Security.addProvider(new KonaCryptoProvider()); // 实现国密密码学算法SM2，SM3和SM4
+        Security.addProvider(new KonaPKIXProvider()); // 实现国密证书的解析与验证，并可加载和创建包含国密证书的密钥库
+        Security.addProvider(new KonaSSLProvider()); // 实现中国的传输层密码协议（TLCP）
+        // 加载可以信任的服务器证书
+        KeyStore trustStore = KeyStore.getInstance("PKCS12", "KonaPKIX");
+        trustStore.load(JavaClient.class.getResourceAsStream("/org/example/server.ks"), "testpasswd".toCharArray());
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("PKIX");
+        tmf.init(trustStore);
+        // 加载客户端证书，服务器通过客户端证书进行验证
+        KeyStore keyStore = KeyStore.getInstance("PKCS12", "KonaPKIX");
+        keyStore.load(JavaClient.class.getResourceAsStream("/org/example/client.ks"), "testpasswd".toCharArray());
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("PKIX");
+        kmf.init(keyStore, "testpasswd".toCharArray());
+
+        // 创建SSLContext
+        SSLContext context = SSLContext.getInstance("TLCPv1.1");
+        context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+
+        SSLSocket socket = (SSLSocket) context.getSocketFactory().createSocket("127.0.0.1", 9090);
+
+        return socket;
     }
 }
--- server/src/main/java/org/example/JavaServer.java
+++ server/src/main/java/org/example/JavaServer.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import com.tencent.kona.crypto.KonaCryptoProvider;
+import com.tencent.kona.pkix.KonaPKIXProvider;
+import com.tencent.kona.ssl.KonaSSLProvider;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TThreadPoolServer;
 import org.apache.thrift.transport.TServerSocket;
@@ -10,6 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import tutorial.Calculator;
+
+import javax.net.ssl.*;
+import java.security.KeyStore;
+import java.security.SecureRandom;
+import java.security.Security;
 
 @SpringBootApplication
 public class JavaServer implements ApplicationListener<ApplicationReadyEvent> {
@@ -38,7 +46,7 @@ public class JavaServer implements ApplicationListener<ApplicationReadyEvent> {
 
     public void simple(Calculator.Processor processor) {
         try {
-            TServerTransport serverTransport = new TServerSocket(9090);
+            TServerTransport serverTransport = new TServerSocket(getSSLServerSocket());
             // Use this for a multithreaded server
             TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
             System.out.println("Starting the simple server...");
@@ -46,5 +54,32 @@ public class JavaServer implements ApplicationListener<ApplicationReadyEvent> {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public SSLServerSocket getSSLServerSocket() throws Exception {
+        // 加载国密Provider
+        Security.addProvider(new KonaCryptoProvider()); // 实现国密密码学算法SM2，SM3和SM4
+        Security.addProvider(new KonaPKIXProvider()); // 实现国密证书的解析与验证，并可加载和创建包含国密证书的密钥库
+        Security.addProvider(new KonaSSLProvider()); // 实现中国的传输层密码协议（TLCP）
+        // 加载可以信任的客户端证书
+        KeyStore trustStore = KeyStore.getInstance("PKCS12", "KonaPKIX");
+        trustStore.load(JavaServer.class.getResourceAsStream("/org/example/client.ks"), "testpasswd".toCharArray());
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("PKIX");
+        tmf.init(trustStore);
+        // 加载服务器证书，客户端通过服务器证书进行验证
+        KeyStore keyStore = KeyStore.getInstance("PKCS12", "KonaPKIX");
+        keyStore.load(JavaServer.class.getResourceAsStream("/org/example/server.ks"), "testpasswd".toCharArray());
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance("PKIX");
+        kmf.init(keyStore, "testpasswd".toCharArray());
+
+        // 创建SSLContext
+        SSLContext context = SSLContext.getInstance("TLCPv1.1");
+        context.init(kmf.getKeyManagers(), tmf.getTrustManagers(), new SecureRandom());
+
+        SSLServerSocketFactory sslServerSocketFactory = context.getServerSocketFactory();
+        SSLServerSocket sslServerSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(9090);
+        sslServerSocket.setNeedClientAuth(true); // 服务器需要验证客户端身份，通过信任的客户端证书（TrustManagerFactory）
+
+        return sslServerSocket;
     }
 }
